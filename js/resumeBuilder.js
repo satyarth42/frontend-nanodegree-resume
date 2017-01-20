2306648d5fa9073d36a95d34cfa41a241b9f61ee
satyarth <satyarth.ojha@gmail.com>
2017-01-20 23:45:28 +0530

    day 1 updates

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,7 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var formattedName = HTMLheaderName.replace("%data%","Satyarth Ojha");
+var formattedRole = HTMLheaderRole.replace("%data%","Web Developer");
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
